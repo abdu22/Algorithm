24995db232b844ee5761158fe7bd94c2809cfeb0
abdurahman <35825332+abdu22@users.noreply.github.com>
2019-08-20 00:48:53 -0700

    Update FindAllAnagram.java

--- Arrays And Strings/src/FindAllAnagram.java
+++ Arrays And Strings/src/FindAllAnagram.java
@@ -5,14 +5,30 @@ public class FindAllAnagram {
 	
    public List<Integer> findAnagrams(String s, String p) {
 	    
-	   char a[] = s.toCharArray();
-	   char b[] = p.toCharArray();
-	   
-	   for(int i = 0; i <a.length ; i++) {
-		   
-	   }
-	   
-        return null;
+  List<Integer> l = new ArrayList<>();
+         if (s == null || s.length() < 1 || p.length() > s.length()) {
+             return l;
+         }
+         int [] aP = new int[26];
+         for(int i=0; i <p.length(); i++) {
+             aP[p.charAt(i)-'a']++;
+         }
+         for(int i = 0; i < s.length()-p.length() +1 ; i++){
+
+             int []aS = new int[26];
+             for (int j = i; j< (i+p.length()); j++){
+                 aS[s.charAt(j)-'a']++;
+             }
+             if(isMatch(aP,aS)) l.add(i);
+         }
+       return l;
+    }
+
+    public static boolean isMatch(int [] a, int []b) {
+        for (int i = 0; i < a.length; i++) {
+            if(a[i] != b[i]) return  false;
+        }
+        return true;
     }
 	public static void main(String[] args) {
 		
